ad6c62e24ae00d1e0800c58265a895285a1c2d7c
Bruno Gomes <brunoshy@gmail.com>
2025-04-24 06:23:01 +0000

    fix install ad role

--- scripts/install_ad_role.ps1
+++ scripts/install_ad_role.ps1
@@ -1,47 +1,31 @@
-# Script para instalar a role de Active Directory em um Windows Server limpo
-
+# Script para instalar a role de Active Directory em um Windows Server limpinho
 param(
-    [string]$DomainSuffix = ".lan",
-    [ValidateSet('Win2008','Win2008R2','Win2012','Win2012R2','Win2016','Win2019','WinThreshold')] 
-    [string]$ForestMode = 'Win2016',
-    [ValidateSet('Win2008','Win2008R2','Win2012','Win2012R2','Win2016','Win2019','WinThreshold')] 
-    [string]$DomainMode = 'Win2016',
-    [securestring]$SafeModeAdministratorPassword = (Read-Host -Prompt "Digite a senha do DSRM (modo de segurança de restauração de serviços de diretório)" -AsSecureString),
-    [switch]$InstallDNS = $true,
-    [switch]$CreateDnsDelegation = $false,
-    [string]$DatabasePath = "C:\\Windows\\NTDS",
-    [string]$LogPath = "C:\\Windows\\NTDS",
-    [string]$SysvolPath = "C:\\Windows\\SYSVOL",
-    [string]$SiteName = $env:COMPUTERNAME
+  [Parameter(Mandatory=$false)]
+  [string]$DomainNetbiosName         = $env:COMPUTERNAME,
+  [string]      $DomainSuffix        = ".lan",
+  [string]      $DomainName          = "$($DomainNetbiosName)$($DomainSuffix)",
+  [string]      $DatabasePath        = "C:\Windows\NTDS",
+  [string]      $SysvolPath          = "C:\Windows\SYSVOL",
+  [string]      $LogPath             = "C:\Windows\NTDS",
+  [string]      $SafeModeAdministratorPassword = "P@ssw0rd!"
 )
 
-# Obter o hostname atual do servidor
-$FQDN = "$($env:COMPUTERNAME).$($DomainSuffix)"
-
-# Instalar a role de Active Directory
-Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
-
-# Importar o módulo de implantação do AD
+Import-Module ServerManager
+Install-WindowsFeature AD-Domain-Services -IncludeAllSubFeature -IncludeManagementTools
+Install-WindowsFeature DNS -IncludeAllSubFeature -IncludeManagementTools
 Import-Module ADDSDeployment
+Import-Module DnsServer
 
-# Parâmetros da floresta e domínio
-$FullDomainName = "$($env:COMPUTERNAME)$($DomainSuffix)"
-Write-Host "Configurando domínio: $FullDomainName"
-
-# Instalação e promoção
-Install-ADDSForest \
-    -DomainName $FullDomainName \
-    -ForestMode $ForestMode \
-    -DomainMode $DomainMode \
-    -SafeModeAdministratorPassword $SafeModeAdministratorPassword \
-    -InstallDNS:$InstallDNS \
-    -CreateDnsDelegation:$CreateDnsDelegation \
-    -DatabasePath $DatabasePath \
-    -LogPath $LogPath \
-    -SysvolPath $SysvolPath \
-    -SiteName $SiteName \
-    -Force \
-    -NoRebootOnCompletion
+Install-ADDSForest `
+  -DomainName $DomainName `
+  -DomainNetbiosName $DomainNetbiosName `
+  -DatabasePath $DatabasePath `
+  -SysvolPath $SysvolPath `
+  -LogPath $LogPath `
+  -SafeModeAdministratorPassword (ConvertTo-SecureString $SafeModeAdministratorPassword -AsPlainText -Force)
 
 # Reiniciar o servidor para concluir a configuração
+Write-Host "Reboot..."
+pause
 Restart-Computer -Force
+
