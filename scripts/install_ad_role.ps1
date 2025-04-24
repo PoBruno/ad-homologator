058e3347f470d8cb5a55e06a6f441afbc6c28aa4
Bruno Gomes <brunoshy@gmail.com>
2025-04-24 06:40:30 +0000

    fix install feat role addc

--- scripts/install_ad_role.ps1
+++ scripts/install_ad_role.ps1
@@ -1,18 +1,18 @@
 # Script para instalar a role de Active Directory em um Windows Server limpinho
 param(
   [Parameter(Mandatory=$false)]
-  [string]$DomainNetbiosName         = $env:COMPUTERNAME,
-  [string]      $DomainSuffix        = ".lan",
-  [string]      $DomainName          = "$($DomainNetbiosName)$($DomainSuffix)",
-  [string]      $DatabasePath        = "C:\Windows\NTDS",
-  [string]      $SysvolPath          = "C:\Windows\SYSVOL",
-  [string]      $LogPath             = "C:\Windows\NTDS",
-  [string]      $SafeModeAdministratorPassword = "P@ssw0rd!"
+  [string]$DomainNetbiosName              = $env:COMPUTERNAME,
+  [string]$DomainSuffix                   = ".lan",
+  [string]$DomainName                     = "$($DomainNetbiosName)$($DomainSuffix)",
+  [string]$DatabasePath                   = "C:\Windows\NTDS",
+  [string]$SysvolPath                     = "C:\Windows\SYSVOL",
+  [string]$LogPath                        = "C:\Windows\NTDS",
+  [string]$SafeModeAdministratorPassword  = "P@ssw0rd!"
 )
 
 Import-Module ServerManager
-Install-WindowsFeature AD-Domain-Services -IncludeAllSubFeature -IncludeManagementTools
-Install-WindowsFeature DNS -IncludeAllSubFeature -IncludeManagementTools
+#Install-WindowsFeature AD-Domain-Services -IncludeAllSubFeature -IncludeManagementTools
+#Install-WindowsFeature DNS -IncludeAllSubFeature -IncludeManagementTools
 Import-Module ADDSDeployment
 Import-Module DnsServer
 
@@ -22,7 +22,8 @@ Install-ADDSForest `
   -DatabasePath $DatabasePath `
   -SysvolPath $SysvolPath `
   -LogPath $LogPath `
-  -SafeModeAdministratorPassword (ConvertTo-SecureString $SafeModeAdministratorPassword -AsPlainText -Force)
+  -SafeModeAdministratorPassword (ConvertTo-SecureString $SafeModeAdministratorPassword -AsPlainText -Force) `
+  -NoRebootOnCompletion
 
 # Reiniciar o servidor para concluir a configuração
 Write-Host "Reboot..."
